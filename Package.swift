cc19641e2f5b7a772855abeedd90bfbda2c31c6e
akarsh <sakarshkumar@gmail.com>
2019-10-15 11:42:30 +0200

    Refactor removal of comments and re-alignment of close bracket
    - removed comments
    - re-alignment of close bracket

--- Package.swift
+++ Package.swift
@@ -13,13 +13,10 @@ let package = Package(
             targets: ["SwiftSpreadsheet"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftSpreadsheet",
             dependencies: [],
-            path: "SwiftSpreadsheet"
-        ),
+            path: "SwiftSpreadsheet"),
     ],
    swiftLanguageVersions:[.v5]
 )
